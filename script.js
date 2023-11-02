455bd13567eaf5b7380ad4af92d16489484507fe
Paul Becker <paulbecker2003@gmail.com>
2023-11-02 21:43:38 +0100

    implementet button logic

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const container = document.querySelector(".container");
+const changeGridBtn = document.querySelector("#changeGrid");
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid-item");
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "blue";
-  });
-  container.appendChild(div);
+changeGridBtn.addEventListener("click", setGridSize);
+
+let gridSize = 4;
+
+createGrid();
+
+function createGrid() {
+    container.innerHTML = '';
+    const itemBasis = `calc(${100 / gridSize}% - 1px)`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-item");
+    div.style.flexBasis = itemBasis;
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "blue";
+    });
+    container.appendChild(div);
+  }
 }
 
+function setGridSize() {
+  gridSize = prompt("Set Grid size.");
+  createGrid();
+}
