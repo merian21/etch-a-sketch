718e4f98ff9c9ace0e959528b62d96a7803f80ad
Paul Becker <paulbecker2003@gmail.com>
2023-11-02 21:17:51 +0100

    divs now change color when moused over

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector(".container");
-const div = document.createElement("div");
-div.classList.add("grid-item");
+
 for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
   div.classList.add("grid-item");
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "blue";
+  });
   container.appendChild(div);
 }
+
